4f7df35c8878649cae5030af4c83064ec3dec7ee
Brock Wrisberg <brock.wrisberg@gmail.com>
2025-02-05 15:51:18 -0600

    finish

--- src/studio3/SieveOfEratosthenes.java
+++ src/studio3/SieveOfEratosthenes.java
@@ -12,7 +12,7 @@ public class SieveOfEratosthenes {
 	//	int[] filledArray = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
 		//		26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40 };
 		int[] primeNumbers;
-		primeNumbers = new int[n-1];
+		primeNumbers = new int[n];
 		int position = 0;
 		for (int k = 2; k <= primeNumbers.length + 1; k++) {
 			primeNumbers[position] = k;
@@ -25,10 +25,15 @@ public class SieveOfEratosthenes {
 			int turn = n / p;
 			for (int i = 1; i <= turn; i++) {
 				int number = p * i;
-	
+				primeNumbers[number + 1] = 1;
+				
+				
 		}
 		}
-			
+		if primeNumbers == 0;
+		for (int i = 0; i < n; i ++);{
+		System.out.println();
+
 
 	}
 }
