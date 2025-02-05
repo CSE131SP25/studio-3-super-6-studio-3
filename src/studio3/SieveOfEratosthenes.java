bda5d42901700e90ac7af63c60743bebdaf4e204
Brock Wrisberg <brock.wrisberg@gmail.com>
2025-02-05 15:51:05 -0600

    studio is finished

---
+++ src/studio3/SieveOfEratosthenes.java
@@ -0,0 +1,34 @@
+package studio3;
+
+import java.util.*;
+
+public class SieveOfEratosthenes {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Up to what value do you want to find a prime number?");
+		
+
+		int n = in.nextInt();
+	//	int[] filledArray = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+		//		26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40 };
+		int[] primeNumbers;
+		primeNumbers = new int[n-1];
+		int position = 0;
+		for (int k = 2; k <= primeNumbers.length + 1; k++) {
+			primeNumbers[position] = k;
+			position++;
+		}
+		System.out.println(Arrays.toString(primeNumbers));
+
+		int j = n + 2;
+		for (int p = 2; p < j; p++) {
+			int turn = n / p;
+			for (int i = 1; i <= turn; i++) {
+				int number = p * i;
+	
+		}
+		}
+			
+
+	}
+}
